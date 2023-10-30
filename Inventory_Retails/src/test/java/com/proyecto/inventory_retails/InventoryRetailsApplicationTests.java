40f8f82602712e8abef7ac510622d6ea4560205c
26Aron <129801527+26Aron@users.noreply.github.com>
2023-10-30 17:20:33 -0500

    Proyecto creado - SpringBoot

---
+++ Inventory_Retails/src/test/java/com/proyecto/inventory_retails/InventoryRetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.inventory_retails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryRetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
